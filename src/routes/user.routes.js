2310adc6b36d4ccb2cfa39aa1fa2a74481417455
Shubhang- Sagar- Shukla <shubhangsagarshukla@gmail.com>
2025-09-11 18:09:27 +0530

    add user channel profile and watch history endpoints

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,27 +1,41 @@
-import {Router} from 'express'
-import {loginUser,logoutUser,registerUser,refreshAccessToken} from '../controllers/user.controller.js'
+import { Router } from 'express'
+import {
+    loginUser,
+    logoutUser,
+    registerUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
+} from '../controllers/user.controller.js'
 import { upload } from '../middlewares/multer.middleware.js'
 import { verifyJWT } from '../middlewares/auth.middleware.js'
 
-const router=Router()
+const router = Router()
 
 // post ke baad middleware lagana hai ,registerUser se pehle toh its done like this 
 //upload.fields([])  // this is a middleware from multer to handle multiple files upload
-router.route("/register").post( upload.fields([
+router.route("/register").post(upload.fields([
     {
-        name:"avatar",
-        maxCount:1 // maxCount means how many files of this type we are expecting
+        name: "avatar",
+        maxCount: 1 // maxCount means how many files of this type we are expecting
     },
     {
-        name:"coverImage",
-        maxCount:1
-    }  
- 
-
-]),registerUser)
+        name: "coverImage",
+        maxCount: 1
+    }
 
 
-router.route("/login").post(verifyJWT,logoutUser)
+]), registerUser)
+
+
+router.route("/login").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+
+
 
 export default router
