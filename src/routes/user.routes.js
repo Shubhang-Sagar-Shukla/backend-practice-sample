592e477f9e1c14cc723b969fd51401a83d01fda6
Shubhang- Sagar- Shukla <shubhangsagarshukla@gmail.com>
2025-09-11 20:28:03 +0530

    adding more routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -33,9 +33,28 @@ router.route("/register").post(upload.fields([
 ]), registerUser)
 
 
-router.route("/login").post(verifyJWT, logoutUser)
+router.route("/login").post(verifyJWT, loginUser)
+router.route("/logout").post(verifyJWT, logoutUser)
+
+
 router.route("/refresh-token").post(refreshAccessToken)
 
+
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+
+
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+
+router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
+
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+
+router.route("/history").get(verifyJWT,getWatchHistory)
+
+
+router.route()
 
 
 export default router
